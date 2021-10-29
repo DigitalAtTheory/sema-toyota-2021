61b3b42b0193d068223ef016dc68f68e7a872ff7
Josh Suson <josh.suson@gmail.com>
2021-10-29 19:00:11 -0400

    working on mailchimp integration

--- pages/api/digital.js
+++ pages/api/digital.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import mailchimp from "@mailchimp/mailchimp_marketing";
+import md5 from "md5";
 
 module.exports = async (req, res) => {
   const {
@@ -13,6 +15,14 @@ module.exports = async (req, res) => {
     oil_change,
     personal_car,
   } = req.body;
+
+  const subscriberHash = md5(email.toLowerCase());
+  let currentUser;
+
+  mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: process.env.MAILCHIMP_SERVER_PREFIX,
+  });
 
   await axios
     .post(
@@ -38,8 +48,44 @@ module.exports = async (req, res) => {
         },
       }
     )
-    .then((data) => console.log(data))
+    .then((data) => console.log(data.data.id))
     .catch((err) => console.error(err));
+
+  try {
+    const getContact = await mailchimp.lists.getListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID,
+      subscriberHash
+    );
+    console.log(getContact.id);
+    currentUser = true;
+  } catch {
+    console.log("There is no user");
+    currentUser = false;
+  }
+
+  if (!currentUser) {
+    console.log("Adding user");
+    const addContact = await mailchimp.lists.addListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID,
+      {
+        email_address: email,
+        status: "subscribed",
+      }
+    );
+    console.log(addContact.id);
+  }
+  console.log("Adding to customer journey");
+  const joinCustomerJourney = await mailchimp.customerJourneys.trigger(
+    process.env.MAILCHIMP_DIGITAL_JOURNEY,
+    process.env.MAILCHIMP_DIGITAL_STEP,
+    {
+      email_address: email,
+    }
+  );
+
+  console.log(joinCustomerJourney);
+
+  console.log(req.body);
 
   res.status(200).send("Logged");
 };
